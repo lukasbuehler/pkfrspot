10327722a1f7d63994b9a267b63cfa27e067adf5
Lukas Bühler <lukasmc6@gmail.com>
2019-05-21 21:04:27 +0200

    made map work with googlemaps @types

--- parkour-base-angular/src/app/map-page/map-page.component.ts
+++ parkour-base-angular/src/app/map-page/map-page.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { keys } from "../../environments/keys"
+
+import { } from "googlemaps"
 
 @Component({
   selector: 'app-map-page',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapPageComponent implements OnInit {
 
-  constructor() { }
+  google_maps_api_key: string = keys.google_maps;
 
-  ngOnInit() {
+  @ViewChild("map") mapElement: any;
+  map: google.maps.Map;
+
+  constructor()
+  {
   }
 
+  ngOnInit()
+  {
+    var mapProp = {
+      center: new google.maps.LatLng(-34.397, 150.644),
+      zoom: 8,
+      mapTypeId: google.maps.MapTypeId.SATELLITE
+    };
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapProp);
+    /*
+    <!-- Script for the Google Maps API -->
+    <script>
+      function initMap()
+      {
+        new google.maps.Map(document.getElementById('map'), {
+          center: { lat: -34.397, lng: 150.644 },
+          zoom: 8
+        });
+      }
+     */
+  }
 }
