cddb3835806042b4883679d841f4aaf18225a7d1
Lukas Bühler <lukasmc6@gmail.com>
2019-05-21 23:04:06 +0200

    added some posts for the home page

--- parkour-base-angular/src/app/home-page/home-page.component.ts
+++ parkour-base-angular/src/app/home-page/home-page.component.ts
@@ -5,11 +5,39 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss']
 })
-export class HomePageComponent implements OnInit {
+export class HomePageComponent implements OnInit
+{
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit()
+  {
   }
 
+  posts = [
+    {
+      title: "Doggo 1",
+      username: "Shiiiiba",
+      image_src: "https://material.angular.io/assets/img/examples/shiba2.jpg",
+      text: "Wuff"
+    },
+    {
+      title: "Doggo 1",
+      username: "Shiiiiba",
+      image_src: "https://material.angular.io/assets/img/examples/shiba2.jpg",
+      text: "Wuff"
+    },
+    {
+      title: "Doggo 1",
+      username: "Shiiiiba",
+      image_src: "https://material.angular.io/assets/img/examples/shiba2.jpg",
+      text: "Wuff"
+    },
+    {
+      title: "Doggo 1",
+      username: "Shiiiiba",
+      image_src: "https://material.angular.io/assets/img/examples/shiba2.jpg",
+      text: "Wuff"
+    }
+  ]
 }
