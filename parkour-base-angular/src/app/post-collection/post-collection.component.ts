7acd33ed44ffd88966a9169e8e84bf35da9cd798
Lukas Bühler <lukasmc6@gmail.com>
2019-05-26 23:01:37 +0200

    more small changes

--- parkour-base-angular/src/app/post-collection/post-collection.component.ts
+++ parkour-base-angular/src/app/post-collection/post-collection.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import * as moment from "moment"
 
 @Component({
   selector: 'app-post-collection',
@@ -12,6 +13,27 @@ export class PostCollectionComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  // this gets called to often...
+  getPostTime(post: any)
+  {
+    return moment.duration(post.time_posted.seconds, "seconds").subtract((new Date()).getTime(), "milliseconds").humanize(true);
+  }
+
+  likeButtonPress(post: any)
+  {
+    if(post.liked_by_user) {
+      // The post is liked => unlike
+      post.liked_by_user = false;
+      post.likes--;
+    }  
+    else
+    {
+      // The post is not liked => like
+      post.liked_by_user = true;
+      post.likes++;
+    }
   }
 
 }
