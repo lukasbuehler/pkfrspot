020f9ee14f4fec278fab9cf269da63a1304fd4c5
Lukas Bühler <mail@lukasbuehler.ch>
2020-03-06 16:46:05 +0100

    up and running again on new machine

--- parkour-base-angular/src/app/database.service.ts
+++ parkour-base-angular/src/app/database.service.ts
@@ -1,127 +1,124 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 import { keys } from "src/environments/keys";
 
 import { Post, PostSchema } from "src/scripts/db/Post";
 import { Spot } from "src/scripts/db/Spot";
 
-import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import {
+  AngularFirestore,
+  AngularFirestoreCollection
+} from "@angular/fire/firestore";
 
-import { Observable } from 'rxjs';
+import { Observable } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class DatabaseService {
+  constructor(private db: AngularFirestore) {}
 
-  constructor(private db: AngularFirestore) { }
-
-  addPost(newPost: PostSchema)
-  {
-    this.db.collection("posts").add(newPost).then(
-      docRef =>
-      {
-        console.log("Post Document written with ID: "+docRef.id);
-      }
-    ).catch(
-      error =>
-      {
+  addPost(newPost: PostSchema) {
+    this.db
+      .collection("posts")
+      .add(newPost)
+      .then(docRef => {
+        console.log("Post Document written with ID: " + docRef.id);
+      })
+      .catch(error => {
         console.error("Error adding Post Document: ", error);
-      }
-    );
+      });
   }
 
-  getPostUpdates(): Observable<any[]>
-  {
+  getPostUpdates(): Observable<any[]> {
     return new Observable<any[]>(observer => {
-      this.db.collection("posts").get().subscribe((querySnapshot) =>
-      {
-        let posts = [];
+      this.db
+        .collection("posts")
+        .get()
+        .subscribe(
+          querySnapshot => {
+            let posts = [];
 
-        querySnapshot.forEach((doc) => {
-          posts.push(doc.data());
-        });
+            querySnapshot.forEach(doc => {
+              posts.push(doc.data());
+            });
 
-        observer.next(posts);
-        observer.complete();
-      },
-      error => {
-        observer.error(error);
-      },
-      () => {})
-    });
-  }
-
-  getTrendingPosts()
-  {
-
-  }
-
-  getTestSpots(): Observable<Spot.Class[]>
-  {
-    return new Observable<Spot.Class[]>(observer =>
-    {
-      this.db.collection("spots").get().subscribe((querySnapshot) =>
-      {
-        let spots: Spot.Class[] = [];
-
-        console.log("Hello")
-        querySnapshot.forEach((doc) =>
-        {
-          if (doc.data() as Spot.Schema)
-          {
-            let newSpot: Spot.Class = new Spot.Class(doc.id, doc.data() as Spot.Schema);
-            console.log(newSpot);
-
-            spots.push(newSpot);
-          }
-          else
-          {
-            console.error("Spot could not be cast to Spot.Schema!");
+            observer.next(posts);
             observer.complete();
-          }
-        });
-
-        observer.next(spots);
-        observer.complete();
-      },
-        error =>
-        {
-          observer.error(error);
-          observer.complete();
-        },
-        () => { })
+          },
+          error => {
+            observer.error(error);
+          },
+          () => {}
+        );
     });
   }
 
-  getSpotSearch(searchString: string): Observable<Spot.Class[]>
-  {
+  getTrendingPosts() {}
+
+  getTestSpots(): Observable<Spot.Class[]> {
+    return new Observable<Spot.Class[]>(observer => {
+      this.db
+        .collection("spots")
+        .get()
+        .subscribe(
+          querySnapshot => {
+            let spots: Spot.Class[] = [];
+
+            querySnapshot.forEach(doc => {
+              if (doc.data() as Spot.Schema) {
+                let newSpot: Spot.Class = new Spot.Class(
+                  doc.id,
+                  doc.data() as Spot.Schema
+                );
+                console.log(newSpot);
+
+                spots.push(newSpot);
+              } else {
+                console.error("Spot could not be cast to Spot.Schema!");
+                observer.complete();
+              }
+            });
+
+            observer.next(spots);
+            observer.complete();
+          },
+          error => {
+            observer.error(error);
+            observer.complete();
+          },
+          () => {}
+        );
+    });
+  }
+
+  getSpotSearch(searchString: string): Observable<Spot.Class[]> {
     return new Observable<any[]>(observer => {
-      this.db.collection("spots").get()
-    })
+      this.db.collection("spots").get();
+    });
   }
 
-  getSpotsOnMap()
-  {
+  getSpotsOnMap() {}
 
-  }
-
-  setSpot(spot: Spot.Class): Observable<any>
-  {
+  setSpot(spot: Spot.Class): Observable<any> {
     let spotId: string = spot.id;
 
     return new Observable<any>(observer => {
-      this.db.collection("spots").doc(spot.id).set(spot.data).then(
-        /* fulfilled */ value => {
-          observer.next(value);
-          observer.complete();
-        },
-        /* rejected */ reason => {
-          observer.error(reason);
-        }
-        ).catch(error => {
+      this.db
+        .collection("spots")
+        .doc(spot.id)
+        .set(spot.data)
+        .then(
+          /* fulfilled */ value => {
+            observer.next(value);
+            observer.complete();
+          },
+          /* rejected */ reason => {
+            observer.error(reason);
+          }
+        )
+        .catch(error => {
           observer.error(error);
-        })
-    })
+        });
+    });
   }
-  
 }
---
+++ parkour-base-angular/src/environments/keys_example.ts
@@ -0,0 +1,6 @@
+export const keys = {
+  google_maps: "... Maps JavaScript API (Server key)",
+  firebaseConfig: {
+    // the config found in the firebase console
+  }
+};
