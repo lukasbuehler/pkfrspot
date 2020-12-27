3129b19594023097b962b2694410732c6d1e46aa
Lukas Bühler <mail@lukasbuehler.ch>
2020-12-27 15:48:31 +0100

    angular update to version 11

--- parkour-base-angular/src/app/sign-in-page/sign-in-page.component.spec.ts
+++ parkour-base-angular/src/app/sign-in-page/sign-in-page.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { SignInPageComponent } from './sign-in-page.component';
 
@@ -6,7 +6,7 @@ describe('SignInPageComponent', () => {
   let component: SignInPageComponent;
   let fixture: ComponentFixture<SignInPageComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ SignInPageComponent ]
     })
--- parkour-base-angular/src/app/spot-card/spot-card.component.spec.ts
+++ parkour-base-angular/src/app/spot-card/spot-card.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { SpotCardComponent } from './spot-card.component';
 
@@ -6,7 +6,7 @@ describe('SpotCardComponent', () => {
   let component: SpotCardComponent;
   let fixture: ComponentFixture<SpotCardComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ SpotCardComponent ]
     })
