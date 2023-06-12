f7c4ab397b41342defbfd39aed0b2422436882f3
Lukas Bühler <mail@lukasbuehler.ch>
2023-06-12 21:43:23 +0200

    made bottom sheet super cool

--- parkour-base-angular/src/app/bottom-sheet/bottom-sheet.component.ts
+++ parkour-base-angular/src/app/bottom-sheet/bottom-sheet.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { CdkDragDrop } from "@angular/cdk/drag-drop/index.js";
+import { Component, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-bottom-sheet',
-  templateUrl: './bottom-sheet.component.html',
-  styleUrls: ['./bottom-sheet.component.scss']
+  selector: "app-bottom-sheet",
+  templateUrl: "./bottom-sheet.component.html",
+  styleUrls: ["./bottom-sheet.component.scss"],
 })
 export class BottomSheetComponent {
+  @Input() isOpen: boolean = false;
+  @Input() title: string = "";
 
+  onDrop(event: CdkDragDrop<string[]>) {
+    if (event.previousIndex > event.currentIndex) {
+      // panel was opened (moved up)
+      this.isOpen = true;
+    } else if (event.previousIndex < event.currentIndex) {
+      // panel was closed (moved down)
+      this.isOpen = false;
+    }
+  }
 }
