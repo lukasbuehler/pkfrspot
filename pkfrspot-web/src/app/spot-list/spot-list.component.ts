d8d7f328fdac0ff70d3f5f518694e0a592a5722c
Lukas Bühler <me@lukasbuehler.ch>
2024-04-29 14:55:24 +0200

    added toggle for spot list

--- pkfrspot-web/src/app/spot-list/spot-list.component.ts
+++ pkfrspot-web/src/app/spot-list/spot-list.component.ts
@@ -1,11 +1,13 @@
 import { Component, EventEmitter, Input, Output } from "@angular/core";
 import { Spot } from "src/scripts/db/Spot.js";
 import { SpotPreviewCardComponent } from "../spot-preview-card/spot-preview-card.component";
+import { MatButtonToggleModule } from "@angular/material/button-toggle";
+import { MatIconModule } from "@angular/material/icon";
 
 @Component({
   selector: "app-spot-list",
   standalone: true,
-  imports: [SpotPreviewCardComponent],
+  imports: [SpotPreviewCardComponent, MatButtonToggleModule, MatIconModule],
   templateUrl: "./spot-list.component.html",
   styleUrl: "./spot-list.component.scss",
 })
