1fea428681e684c441c81dbcc482d738ea0a5938
Lukas Bühler <me@lukasbuehler.ch>
2024-06-11 21:03:39 +0200

    added devkit core to node_modules

--- build_test/test.mjs
+++ build_test/test.mjs
@@ -5,12 +5,22 @@ async function demonstrate() {
   const host = workspaces.createWorkspaceHost(new NodeJsSyncHost());
   const { workspace } = await workspaces.readWorkspace("../", host);
 
+  console.log(workspace.projects);
+
   const apps = [];
   workspace.projects.forEach((value, key) => {
     if (value.extensions.projectType === "application") apps.push(key);
   });
 
   console.log(apps);
+
+  const project = apps[0];
+  if (apps.length > 1 || !project)
+    throw new Error("Unable to determine the application to deploy");
+
+  const workspaceProject = workspace.projects.get(project);
+
+  console.log(workspaceProject.targets);
 }
 
 demonstrate();
