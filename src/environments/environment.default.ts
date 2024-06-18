8c8c62cd51be6634e1579c9926f0bdd107dfef5e
Lukas Bühler <me@lukasbuehler.ch>
2024-06-18 20:14:35 +0200

    maybe one of these does it

--- src/environments/environment.default.ts
+++ src/environments/environment.default.ts
@@ -10,7 +10,7 @@ export const environment = {
       appId: "1:294969617102:web:f9a2fcf843e8b288313e9f",
       databaseURL: "https://parkour-base-project.firebaseio.com",
       storageBucket: "parkour-base-project.appspot.com",
-      locationId: "europe-west",
+      locationId: "us-central1",
       authDomain: "parkour-base-project.firebaseapp.com",
       messagingSenderId: "294969617102",
       measurementId: "G-K7E4HFP8NM",
