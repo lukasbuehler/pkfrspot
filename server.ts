12d662fbf2622d8e8163a53aa84c1cc89d9e5dd2
Lukas Bühler <me@lukasbuehler.ch>
2024-07-22 09:32:57 +0200

    port

--- server.ts
+++ server.ts
@@ -47,7 +47,7 @@ export function app(): express.Express {
 }
 
 function run(): void {
-  const port = process.env["PORT"] || 4200;
+  const port = process.env["PORT"] || 8080;
 
   // Start up the Node server
   const server = app();
