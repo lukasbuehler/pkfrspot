be2237f9f3e1a8d2775460554d700ef1af60ecdc
Lukas Bühler <me@lukasbuehler.ch>
2024-08-26 14:39:24 +0200

    WIP added more i18n fields

--- src/proxy-server.mjs
+++ src/proxy-server.mjs
@@ -4,9 +4,36 @@ import { app as serverDeCH } from "./server/de-CH/server.mjs";
 
 const express = require("express");
 
+function detectLanguage(req, res, next) {
+  const acceptLanguage = req.headers["accept-language"];
+  if (!acceptLanguage) {
+    return next();
+  }
+
+  const languages = acceptLanguage.split(",").map((lang) => lang.split(";")[0]);
+  const supportedLanguages = ["en", "de", "de-CH"];
+  const defaultLanguage = "en";
+
+  const preferredLanguage =
+    languages.find((lang) => supportedLanguages.includes(lang)) ||
+    defaultLanguage;
+
+  if (
+    !req.path.startsWith("/en") &&
+    !req.path.startsWith("/de") &&
+    !req.path.startsWith("/de-CH")
+  ) {
+    return res.redirect(`/${preferredLanguage}${req.path}`);
+  }
+
+  next();
+}
+
 function run() {
   const port = process.env.PORT || 8080;
   const server = express();
+
+  server.use(detectLanguage);
 
   server.use("/en", serverEn());
   server.use("/de", serverDe());
