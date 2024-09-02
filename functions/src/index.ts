fa633c9fa970aca3c254865b5fa0dcbf78e3959e
Lukas Bühler <me@lukasbuehler.ch>
2024-09-02 13:06:54 +0200

    updated cloud functions for reverse geocoding

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,18 +1,29 @@
-import * as functions from "firebase-functions/v1"; // TODO update to v2
 import * as admin from "firebase-admin";
+import {
+  onDocumentWritten,
+  onDocumentCreated,
+} from "firebase-functions/v2/firestore";
+import { defineSecret } from "firebase-functions/params";
+
+import { setGlobalOptions } from "firebase-functions/v2/options";
+setGlobalOptions({ maxInstances: 10 });
+
 import { GeoPoint } from "firebase-admin/firestore";
 
-admin.initializeApp(functions.config().firebase);
+admin.initializeApp();
+
+// get the google API Key from the secret manager
+const googleAPIKey = defineSecret("GOOGLE_API_KEY");
 
 /**
  * This function counts all the likes on one post and updates the number of likes on the post document.
  * It is run everytime a like is added, updated or removed.
  */
-export const countPostLikesOnWrite = functions.firestore
-  .document("posts/{postId}/likes/{likeId}")
-  .onWrite((change, context) => {
+export const countPostLikesOnWrite = onDocumentWritten(
+  "posts/{postId}/likes/{likeId}",
+  async (event) => {
     //const likeId = context.params.likeId;
-    const likedPostId = context.params.postId;
+    const likedPostId = event.params.postId;
 
     const postRef = admin.firestore().collection("posts").doc(likedPostId);
 
@@ -24,15 +35,16 @@ export const countPostLikesOnWrite = functions.firestore
 
         return postRef.update({ like_count: likeCount });
       });
-  });
+  }
+);
 
 /**
  * This function counts all the comments on one post and updates the number of comments on the post document.
  */
-export const countFollowersOnWrite = functions.firestore
-  .document("users/{userId}/followers/{followerId}")
-  .onWrite((change, context) => {
-    const userId = context.params.userId;
+export const countFollowersOnWrite = onDocumentWritten(
+  "users/{userId}/followers/{followerId}",
+  async (event) => {
+    const userId = event.params.userId;
 
     const userRef = admin.firestore().collection("users").doc(userId);
 
@@ -44,15 +56,16 @@ export const countFollowersOnWrite = functions.firestore
 
         return userRef.update({ follower_count: followerCount });
       });
-  });
+  }
+);
 
 /**
  * This function counts all the following on one user and updates the number of following on the user document.
  */
-export const countFollowingOnWrite = functions.firestore
-  .document("users/{userId}/following/{followingId}")
-  .onWrite((change, context) => {
-    const userId = context.params.userId;
+export const countFollowingOnWrite = onDocumentWritten(
+  "users/{userId}/following/{followingId}",
+  async (event) => {
+    const userId = event.params.userId;
 
     const userRef = admin.firestore().collection("users").doc(userId);
 
@@ -64,7 +77,8 @@ export const countFollowingOnWrite = functions.firestore
 
         return userRef.update({ following_count: followingCount });
       });
-  });
+  }
+);
 
 interface PartialSpotSchema {
   location: GeoPoint;
@@ -96,9 +110,9 @@ interface SpotClusterTile {
 /*
  * Cluster all spots
  */
-export const clusterAllSpots = functions.firestore
-  .document("spot_clusters/run")
-  .onCreate((snap, context) => {
+export const clusterAllSpots = onDocumentCreated(
+  "spot_clusters/run",
+  async (event) => {
     // define clustering function
     const getClusterTilesForAllSpots = (
       allSpots: PartialSpotSchema[]
@@ -245,7 +259,8 @@ export const clusterAllSpots = functions.firestore
               });
           });
       });
-  });
+  }
+);
 
 // export const clusterSpotsOnWrite = functions.firestore
 //   .document("spots/{spotId}")
@@ -273,58 +288,162 @@ export const clusterAllSpots = functions.firestore
 //     }
 //   });
 
-type PartialSpotWithLocales = {
-  name?: {
-    [locale: string]: string;
+type AddressType = {
+  sublocality?: string;
+  locality?: string;
+  country?: {
+    code: string; // alpha 2
+    name: string;
   };
-  description?: {
-    [locale: string]: string;
-  };
+  formatted?: string;
 };
 
-// fix to replace the underscore in the name and description locale keys with
-// hyphens (e.g de_CH to de-CH)
-export const fixLocaleKeys = functions.firestore
-  .document("spots/run-underscore-fix")
-  .onCreate(async (snap, context) => {
-    // 1. Load ALL spots
-    const spotsSnap = await admin.firestore().collection("spots").get();
-    const spots = spotsSnap.docs;
+type ResultType = {
+  address_components: {
+    long_name: string;
+    short_name: string;
+    types: string[];
+  }[];
+  formatted_address: string;
+  geometry: any;
+  place_id: string;
+};
 
-    // 2. Update all spots
-    const updateBatch = admin.firestore().batch();
+const updateSpotAddressFromLocation = async (
+  location: GeoPoint,
+  apiKey: string
+): Promise<AddressType> => {
+  if (!location || !location.latitude || !location.longitude) {
+    return Promise.reject("Location is invalid");
+  }
+  if (!apiKey) {
+    return Promise.reject("Google API Key is missing");
+  }
 
-    spots.forEach((doc) => {
-      const spot = doc.data() as PartialSpotWithLocales;
-      const updatedSpot: PartialSpotWithLocales = {};
+  const lat = location.latitude;
+  const lng = location.longitude;
 
-      if (spot.name) {
-        updatedSpot.name = {};
-        Object.keys(spot.name).forEach((locale) => {
-          const newLocale: string = locale.replace(/_/g, "-");
-          updatedSpot.name![newLocale] = spot.name![locale];
-        });
+  const response = await fetch(
+    `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${apiKey}&` +
+      `result_type=street_address|country|locality|sublocality`
+  ).catch((err) => {
+    console.error("Error in reverse geocoding request", err);
+    return Promise.reject(err);
+  });
+  const data = await response.json().catch((err) => {
+    console.error("Error parsing reverse geocoding response", err);
+    return Promise.reject(err);
+  });
+
+  const address: AddressType = {};
+
+  // loop over the results
+  for (const result of data.results as ResultType[]) {
+    // set the formatted address if not set yet
+    if (!address.formatted) {
+      address.formatted = result.formatted_address;
+    }
+
+    for (const component of result.address_components) {
+      // set the country if not set yet
+      if (!address.country && component.types.includes("country")) {
+        address.country = {
+          code: component.short_name,
+          name: component.long_name,
+        };
+      }
+      // set the locality if not set yet
+      else if (!address.locality && component.types.includes("locality")) {
+        address.locality = component.short_name;
+      }
+      // set the sublocality if not set yet
+      else if (
+        !address.sublocality &&
+        component.types.includes("sublocality")
+      ) {
+        address.sublocality = component.short_name;
+      } else if (
+        !!address.country &&
+        !!address.locality &&
+        !!address.sublocality
+      ) {
+        break;
+      } else {
+        continue;
+      }
+    }
+    if (
+      !!address.formatted &&
+      !!address.country &&
+      !!address.locality &&
+      !!address.sublocality
+    ) {
+      console.debug(
+        "Found formatted address and all address components, sublocality, locality, country. Exiting early."
+      );
+      break;
+    }
+  }
+
+  return address;
+};
+
+/**
+ * Update the address of a spot when the location changes.
+ * This function is triggered when a spot is written (created, updated or deleted).
+ */
+// export const updateSpotAddressOnWrite = functions.firestore.onDocumentWritten(
+//   "spots/{spotId}",
+//   async (event) => {
+//     // if the location of the spot has changed, call Googles reverse geocoding to get the address.
+//     if (
+//       !(event.data?.before?.data()?.location as GeoPoint).isEqual(
+//         event.data?.after?.data()?.location
+//       )
+//     ) {
+//       const location = event.data?.after?.data()?.location as GeoPoint;
+
+//       const address = await updateSpotAddressFromLocation(location);
+
+//       return event.data?.after?.ref.update({ address: address });
+//     }
+//     return;
+//   }
+// );
+
+/**
+ * Update the addresses of all existing spots.
+ */
+export const updateAllSpotAddresses = onDocumentCreated(
+  { document: "spots/run-update-addresses", secrets: [googleAPIKey] },
+  async (event) => {
+    const spots = await admin.firestore().collection("spots").get();
+    const apiKey: string = googleAPIKey.value();
+
+    for (const spot of spots.docs) {
+      const location = spot.data().location as GeoPoint;
+      if (!location) {
+        console.warn("Spot has no location", spot.id);
+        continue;
+      }
+      if (!location?.latitude || !location?.longitude) {
+        console.warn("Spot location is invalid", spot.id, location);
+        continue;
       }
 
-      if (spot.description) {
-        updatedSpot.description = {};
-        Object.keys(spot.description).forEach((locale) => {
-          const newLocale: string = locale.replace(/_/g, "-");
-          updatedSpot.description![newLocale] = spot.description![locale];
-        });
-      }
+      const address = await updateSpotAddressFromLocation(
+        location,
+        apiKey
+      ).catch((err) => {
+        console.error("Error updating address for spot", spot.id, err);
+        return Promise.reject(err);
+      });
 
-      if (Object.keys(updatedSpot).length === 0) return;
-
-      updateBatch.update(doc.ref, updatedSpot);
-    });
-
-    // Commit the batch
-    await updateBatch.commit();
-    console.log("Locale keys updated successfully");
+      await spot.ref.update({ address: address });
+      console.log("Updated address for spot", spot.id, address);
+    }
 
     // delete the run document
-    await snap.ref.delete();
-
-    return;
-  });
+    return event.data?.ref.delete();
+  }
+);
