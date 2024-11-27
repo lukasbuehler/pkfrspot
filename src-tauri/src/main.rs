dc90919f3d83eab172e2dce9c3de9688b89bc1d1
Lukas Bühler <me@lukasbuehler.ch>
2024-11-27 15:39:40 +0100

    tauri is running

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,11 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+#[tauri::command]
+fn greet(name: &str) -> String {
+   format!("Hello, {}!", name)
+}
+
 fn main() {
   app_lib::run();
 }
